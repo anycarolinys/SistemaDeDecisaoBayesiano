fe8acb90855a5ec5a897081e7081449ec5757cf0
Vinicius Cunha <vinicius.git36@gmail.com>
2023-05-07 23:52:07 -0300

    correção versão pyscript

---
+++ frontend/src/index.py
@@ -0,0 +1,11 @@
+import random
+botao = Element("botaoEnviar")
+campoSaida = Element("resultado")
+campoEntrada = Element("campoTexto")
+
+def obter_valor():
+    valor = campoEntrada.value
+    campoSaida.write(valor)
+
+botao.element.onkeypress = obter_valor
+
