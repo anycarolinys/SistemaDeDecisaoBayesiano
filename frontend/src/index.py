d408b58a55e4d7a1c4e6514d1f417c94ee0aad10
Gustavo-caetano <gustavo.santos@dcomp.ufs.br>
2023-05-10 02:39:30 -0300

    primeira execucao

--- frontend/src/index.py
+++ frontend/src/index.py
@@ -1,16 +1,29 @@
-import random
-import sys
-sys.path.append('/')
+import js
 
-import 
+import decision
 
+rede =decision.DecisionNetworkHealth()
+campoSaida = Element("respostaAspirina")
 
-botao = Element("botaoEnviar")
-campoSaida = Element("resultado")
-campoEntrada = Element("campoTexto")
+nosEvidencia = ["Fever","Therm","Reaction"]
+evidencia ={}
 
 def obter_valor():
-    valor = campoEntrada.value
-    campoSaida.write(valor)
+    evidencia = {}
+    valorFebre = js.document.querySelectorAll("#febre")
+    valorTermometro = js.document.querySelectorAll("#termometro")
+    valorReacao = js.document.querySelectorAll("#aspirina")
+    
+    for i in range(0,2):
+        if(valorFebre[i].checked):
+            evidencia[nosEvidencia[0]] = True if str(valorFebre[i].value) == "True" else False
+        if(valorTermometro[i].checked):
+            evidencia[nosEvidencia[1]] = True if str(valorTermometro[i].value) == "True" else False
+        if(valorReacao[i].checked):
+            evidencia[nosEvidencia[2]] = True if str(valorReacao[i].value) == "True" else False
+    
+    
+    campoSaida.write(rede.best_action(evidencia))
+    
+    
 
-botao.element.onkeypress = obter_valor
